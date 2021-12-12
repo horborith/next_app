a8888a0e98f165f2b0ac8dbd2171a2923f1fff27
horborith <horborith.h@gmail.com>
2021-12-12 12:21:58 +0700

    add styled components

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,9 @@
 import * as React from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import { ServerStyleSheets } from "@mui/styles";
+import { ServerStyleSheet as StyledComponentSheets } from "styled-components";
 
-export default class MyDocument extends Document {
+class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
@@ -24,3 +26,34 @@ export default class MyDocument extends Document {
     );
   }
 }
+
+MyDocument.getInitialProps = async (ctx) => {
+  // Render app and page and get the context of the page with collected side effects.
+  const sheets = new ServerStyleSheets();
+  const styledComponentSheet = new StyledComponentSheets();
+  const originalRenderPage = ctx.renderPage;
+
+  try {
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => (props) =>
+          styledComponentSheet.collectStyles(
+            sheets.collect(<App {...props} />)
+          ),
+      });
+
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return {
+      ...initialProps,
+      // Styles fragment is rendered after the app and page rendering finish.
+      styles: [
+        ...React.Children.toArray(initialProps.styles),
+        sheets.getStyleElement(),
+      ],
+    };
+  } finally {
+    styledComponentSheet.seal();
+  }
+};
+export default MyDocument;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,25 @@
 import type { NextPage } from "next";
 import Container from "@mui/material/Container";
-import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import Fingerprint from "@mui/icons-material/Fingerprint";
+import Avatar from "@mui/material/Avatar";
 import styled from "styled-components";
+import MyButton from "./../src/components/Button";
 
 const Home: NextPage = () => {
   return (
     <Container maxWidth="sm">
       <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Next.js v5-beta with TypeScript example
-        </Typography>
-        <StyledButton variant="contained">Welcome</StyledButton>
-        <IconButton aria-label="fingerprint" color="secondary">
-          <Fingerprint />
-        </IconButton>
+        <MyButton title="Hello" />
       </Box>
     </Container>
   );
 };
 const StyledButton = styled(Button)`
-  && {
-    background-color: #097979;
-  }
+  display: flex;
+  /* background-color: #097979;
+  &:hover {
+    background-color: blue;
+  } */
 `;
 export default Home;
