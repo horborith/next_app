edf0ab76a3a5ec3764d31e1d1d9141142af559f2
horborith <horborith.h@gmail.com>
2022-02-21 08:20:11 +0700

    fixed error

--- utils/playerAPI.ts
+++ utils/playerAPI.ts
@@ -1,73 +1,73 @@
-// class PlayerAPI {
-//   audio: HTMLAudioElement;
-//   private _repeat: boolean;
-//   blockTimeUpdate: boolean;
-//   constructor() {
-//     this.audio = new Audio();
-//     this.blockTimeUpdate = false;
-//     this._repeat = false;
-//     this.audio.addEventListener("ended", () => this.audioOnFinish());
-//     this.audio.addEventListener("timeupdate", () => this.timeUpdate());
-//   }
-//   timeUpdate(): any {
-//     throw new Error("Method not implemented.");
-//   }
+class PlayerAPI {
+  audio: HTMLAudioElement;
+  private _repeat: boolean;
+  blockTimeUpdate: boolean;
+  constructor() {
+    this.audio = new Audio();
+    this.blockTimeUpdate = false;
+    this._repeat = false;
+    this.audio.addEventListener("ended", () => this.audioOnFinish());
+    this.audio.addEventListener("timeupdate", () => this.timeUpdate());
+  }
+  timeUpdate(): any {
+    throw new Error("Method not implemented.");
+  }
 
-//   resumeTrack() {
-//     this.audio.play();
-//   }
+  resumeTrack() {
+    this.audio.play();
+  }
 
-//   pauseTrack() {
-//     this.audio.pause();
-//   }
+  pauseTrack() {
+    this.audio.pause();
+  }
 
-//   playTrack(track: any, context: any) {
-//     if (!track.preview_url) {
-//       this.playNextTrack();
-//       return;
-//     }
+  playTrack(track: any, context: any) {
+    if (!track.preview_url) {
+      this.playNextTrack();
+      return;
+    }
 
-//     this.loadTrack(track.preview_url);
-//     this.audio.play();
-//   }
+    this.loadTrack(track.preview_url);
+    this.audio.play();
+  }
 
-//   playNextTrack() {}
+  playNextTrack() {}
 
-//   playPrevTrack() {}
+  playPrevTrack() {}
 
-//   startOver() {
-//     this.audio.play();
-//   }
+  startOver() {
+    this.audio.play();
+  }
 
-//   loadTrack(src: string) {
-//     this.audio.src = src;
-//   }
+  loadTrack(src: string) {
+    this.audio.src = src;
+  }
 
-//   get currentTime() {
-//     return this.audio.currentTime;
-//   }
+  get currentTime() {
+    return this.audio.currentTime;
+  }
 
-//   get duration() {
-//     return this.audio.duration;
-//   }
+  get duration() {
+    return this.audio.duration;
+  }
 
-//   get currentVolume() {
-//     return this.audio.volume;
-//   }
+  get currentVolume() {
+    return this.audio.volume;
+  }
 
-//   toggleRepeat() {
-//     this._repeat = !this._repeat;
-//   }
+  toggleRepeat() {
+    this._repeat = !this._repeat;
+  }
 
-//   audioOnFinish() {
-//     if (this._repeat) {
-//       this.currentTime = 0;
-//       this.startOver();
-//       return;
-//     }
-//     this.playNextTrack();
-//   }
-// }
+  audioOnFinish() {
+    if (this._repeat) {
+      //   this.currentTime = 0;
+      this.startOver();
+      return;
+    }
+    this.playNextTrack();
+  }
+}
 
-// const playerAPI = new PlayerAPI();
-// export default playerAPI;
+const playerAPI = new PlayerAPI();
+export default playerAPI;
